267c51f87ed4d4e6ac192de4933cb3d91adc4317
igorkll <frerther@gmail.com>
2024-02-04 23:42:22 +0300

    backup

--- main/graphic.c
+++ main/graphic.c
@@ -138,13 +138,13 @@ void graphic_drawTextBox(int x, int y, int sizeX, int sizeY, const char* text, u
         } else {
             int lx = px * (fontX + 1);
             int ly = py * (fontY + 1);
-            if (sizeY > 0 && (ly + fontY) >= sizeY) break;
             if (sizeX > 0 && (lx + fontX) >= sizeX) {
                 px = 0;
                 py = py + 1;
                 lx = px * (fontX + 1);
                 ly = py * (fontY + 1);
             }
+            if (sizeY > 0 && (ly + fontY) >= sizeY) break;
             graphic_drawChar(x + lx, y + ly, chr, color);
             px = px + 1;
         }
