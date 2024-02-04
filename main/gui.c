208b94bf68041ebbd5cb825745e135983a5eab07
igorkll <frerther@gmail.com>
2024-02-05 00:05:07 +0300

    Update gui.c

--- main/gui.c
+++ main/gui.c
@@ -12,10 +12,12 @@ void gui_status(const char* text) {
 }
 
 void gui_splash(const char* text) {
+    int offset = graphic_getFontSizeY() + 1;
+
     graphic_clear(color_black);
-    graphic_drawRect(2, 2, graphic_x() - 4, graphic_y() - 8, color_white);
-    graphic_drawTextBox(4, 4, graphic_x() - 8, graphic_y() - 16, text, color_white);
-    graphic_drawText(2, graphic_y() - 14, "Press Enter", color_white);
+    graphic_drawRect(2, 2, graphic_x() - 4, graphic_y() - 4 - offset, color_white);
+    graphic_drawTextBox(4, 4, graphic_x() - 8, graphic_y() - 8 - offset, text, color_white);
+    graphic_drawText(2, graphic_y() - (offset + 1), "Press Enter", color_white);
     graphic_update();
 
     while (keyboard_isEnter()) yield();
