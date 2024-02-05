e93e2ba513038fefbbc645e761a7823836413c90
igorkll <frerther@gmail.com>
2024-02-05 15:44:42 +0300

    backup

--- main/gui.c
+++ main/gui.c
@@ -14,6 +14,16 @@ bool gui_isEnter() {
     return resultState;
 }
 
+bool moveState[4];
+bool gui_isMoveButton(int index) {
+    bool currentState = keyboard_isMoveButton(index);
+    bool resultState = !moveState[index] && currentState;
+    if (!currentState) {
+        moveState[index] = true;
+    }
+    return resultState;
+}
+
 
 void gui_status(const char* text) {
     graphic_clear(color_black);
@@ -37,16 +47,15 @@ void gui_splash(const char* text) {
 int gui_menu(const char* title, int pointsCount, char* points[]) {
     int fontY = graphic_getFontSizeY();
     int lineY = fontY + 2;
-    int len = graphic_x() - 1;
     int menu = 0;
 
     void draw() {
         graphic_clear(color_black);
         graphic_drawText(1, 1, title, color_white);
-        graphic_line(0, lineY, len, lineY, color_white);
+        graphic_line(0, lineY, graphic_x() - 1, lineY, color_white);
         for (int i = 0; i < pointsCount; i++) {
-            int pos = (lineY * i) + 1;
-            graphic_fillRect(0, pos, len, fontY + 2, i == menu ? color_white : color_black);
+            int pos = (fontY * i) + lineY + 2;
+            graphic_fillRect(0, pos, graphic_x(), fontY + 2, i == menu ? color_white : color_black);
             graphic_drawText(1, pos + 1, points[i], i == menu ? color_black : color_white);
         }
         graphic_update();
@@ -54,9 +63,18 @@ int gui_menu(const char* title, int pointsCount, char* points[]) {
     draw();
 
     while (true) {
-
-        draw();
-
+        if (gui_isEnter()) return menu;
+        if (gui_isMoveButton(0)) {
+            menu = menu - 1;
+            if (menu < 0) menu = 0;
+            draw();
+        }
+        if (gui_isMoveButton(2)) {
+            menu = menu + 1;
+            if (menu >= pointsCount) menu = pointsCount - 1;
+            draw();
+        }
+        
         yield();
     }
 }
--- main/gui.h
+++ main/gui.h
@@ -1,4 +1,5 @@
 bool gui_isEnter();
+bool gui_isMoveButton(int index);
 void gui_status(const char* text);
 void gui_splash(const char* text);
 int gui_menu(const char* title, int pointsCount, char* points[]);
