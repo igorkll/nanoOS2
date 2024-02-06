c4c82d7173f49ed8c0cfdbd4a7f69ec8cd811a11
igorkll <frerther@gmail.com>
2024-02-07 00:09:00 +0300

    Update snake.c

--- main/games/snake.c
+++ main/games/snake.c
@@ -3,19 +3,22 @@
 #include "../graphic.h"
 #include "../gui.h"
 #include "../color.h"
+#include "../drivers/keyboard.h"
 
 void snake_run() {
     int score = 0;
     while (true) {
-        graphic_clear(color_black);
-
         char scoreStr[16];
-        strcpy(scoreStr, "score: ");
-
+        strcpy(scoreStr, "score:");
         char numStr[16];
         itoa(score, numStr, C_SIZE(numStr));
         strcat(scoreStr, numStr);
 
+        graphic_clear(color_black);
         graphic_drawText(2, 2, scoreStr, color_white);
+        graphic_update();
+
+        if (keyboard_isEnter()) return;
+        yield();
     }
 }
