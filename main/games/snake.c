e6db939c0f932b151b5bd716d1e60a20fe384be0
igorkll <frerther@gmail.com>
2024-02-07 13:38:07 +0300

    Update snake.c

--- main/games/snake.c
+++ main/games/snake.c
@@ -6,7 +6,7 @@
 
 void snake_run() {
     int score = 0;
-    int len = 1;
+    int len = 5;
     int speed = 100;
     int tick = 0;
 
@@ -17,11 +17,19 @@ void snake_run() {
     uint16_t* box = malloc(boxSize);
 
     void boxSet(int x, int y, uint16_t val) {
-        box[x + (y + boxSizeY)] = val;
+        box[x + (y * boxSizeX)] = val;
     }
 
     uint16_t boxGet(int x, int y) {
-        return box[x + (y + boxSizeY)];
+        return box[x + (y * boxSizeX)];
+    }
+
+    void gameOver() {
+        graphic_clear(color_black);
+        gui_drawScoreBar(score);
+        graphic_drawTextBox(0, boxOffset, boxSizeX, boxSizeY, "GAMEOVER", color_white);
+        graphic_update();
+        while (!gui_isEnter()) yield();
     }
 
     for (int ix = 0; ix < boxSizeX; ix++) {
@@ -29,7 +37,12 @@ void snake_run() {
             boxSet(ix, iy, 0);
         }
     }
-    boxSet(3, 3, 20);
+
+
+    int direction = 0;
+    int snakePosX = boxSizeX / 2;
+    int snakePosY = boxSizeY / 2;
+    boxSet(snakePosX, snakePosY, 20);
 
     while (true) {
         if (tick % speed == 0) {
@@ -43,12 +56,34 @@ void snake_run() {
                     }
                 }
             }
+            switch (direction) {
+                case 0:
+                    snakePosY--;
+                    break;
+                case 1:
+                    snakePosX++;
+                    break;
+                case 2:
+                    snakePosY++;
+                    break;
+                case 3:
+                    snakePosX--;
+                    break;
+            }
+            if (snakePosX < 0 || snakePosY < 0 || snakePosX >= boxSizeX || snakePosY >= boxSizeY) {
+                gameOver();
+                return;
+            }
+            boxSet(snakePosX, snakePosY, len);
             graphic_update();
         }
 
         if (gui_isEnter()) {
             free(box);
             return;
+        }
+        for (int i = 0; i < 4; i++) {
+            if (gui_isMoveButton(i)) direction = i;
         }
         yield();
         tick = tick + 1;
