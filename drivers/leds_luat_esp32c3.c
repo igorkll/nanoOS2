87bc9cf4458673e0e024b9dde81275d351eeb2d5
igorkll <frerther@gmail.com>
2024-04-02 13:57:43 +0300

    Update leds_luat_esp32c3.c

--- drivers/leds_luat_esp32c3.c
+++ drivers/leds_luat_esp32c3.c
@@ -4,21 +4,45 @@
 #include "driver/ledc.h"
 
 int pins[] = {12, 13};
+#define LEDC_TIMER              LEDC_TIMER_0
+#define LEDC_MODE               LEDC_LOW_SPEED_MODE
+#define LEDC_OUTPUT_IO          (5) // Define the output GPIO
+#define LEDC_CHANNEL            LEDC_CHANNEL_0
+#define LEDC_DUTY_RES           LEDC_TIMER_8_BIT // Set duty resolution to 13 bits
+#define LEDC_DUTY               (4095) // Set duty to 50%. ((2 ** 13) - 1) * 50% = 4095
+#define LEDC_FREQUENCY          (5000) // Frequency in Hertz. Set frequency at 5 kHz
 
 esp_err_t leds_init() {
     esp_err_t ret = ESP_OK;
     esp_err_t lastret;
 
-    for (int i = 0; i < C_SIZE(pins); i++) {
-        lastret = ledc_set_pin(pins[i], LEDC_LOW_SPEED_MODE, i);
-        if (lastret == ESP_OK) { 
-            gpio_set_level(pins[i], 0);
-        } else {
-            ret = lastret;
+    ledc_timer_config_t ledc_timer = {
+        .speed_mode       = LEDC_MODE,
+        .timer_num        = LEDC_TIMER,
+        .duty_resolution  = LEDC_DUTY_RES,
+        .freq_hz          = LEDC_FREQUENCY,
+        .clk_cfg          = LEDC_AUTO_CLK
+    };
+    lastret = ledc_timer_config(&ledc_timer);
+    if (lastret == ESP_OK) { 
+        for (int i = 0; i < C_SIZE(pins); i++) {
+            ledc_channel_config_t ledc_channel = {
+                .speed_mode     = LEDC_MODE,
+                .channel        = i,
+                .timer_sel      = LEDC_TIMER,
+                .intr_type      = LEDC_INTR_DISABLE,
+                .gpio_num       = pins[i],
+                .duty           = 0, // Set duty to 0%
+                .hpoint         = 0
+            };
+            lastret = ledc_channel_config(&ledc_channel);
+            if (lastret != ESP_OK) { 
+                ret = lastret;
+            }
+            if (i >= 7) break;
         }
-        if (i >= 7) {
-            break;
-        }
+    } else {
+        ret = lastret;
     }
 
     return ret;
