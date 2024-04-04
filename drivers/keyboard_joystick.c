8d976ee6407e5aef62355b9396dee9e1da1f00f8
igorkll <frerther@gmail.com>
2024-04-04 13:20:54 +0300

    Update keyboard_joystick.c

--- drivers/keyboard_joystick.c
+++ drivers/keyboard_joystick.c
@@ -2,6 +2,10 @@
 #include "../main/color.h"
 #include "../main/drivers/keyboard.h"
 
+#ifndef KEYBOARD_ROTATE
+#define KEYBOARD_ROTATE 0
+#endif
+
 esp_err_t keyboard_init() {
     pin_up(KEYBOARD_UP,     GPIO_MODE_DEF_INPUT);
     pin_up(KEYBOARD_RIGHT,  GPIO_MODE_DEF_INPUT);
@@ -11,9 +15,23 @@ esp_err_t keyboard_init() {
 }
 
 bool keyboard_isMoveButton(uint8_t index) {
-
+    index = (index + KEYBOARD_ROTATE) % 4;
+    if (index == 0) {
+        return !gpio_get_level(KEYBOARD_UP);
+    } else if (index == 1) {
+        return !gpio_get_level(KEYBOARD_RIGHT);
+    } else if (index == 2) {
+        return !gpio_get_level(KEYBOARD_DOWN);
+    } else if (index == 3) {
+        return !gpio_get_level(KEYBOARD_LEFT);
+    }
+    return false;
 }
 
-bool keyboard_isActionButton(uint8_t index);
-bool keyboard_isEsc();
-bool keyboard_isEnter();
+bool keyboard_isActionButton(uint8_t index) {
+    return keyboard_isEnter();
+}
+
+bool keyboard_isEnter() {
+    return !gpio_get_level(KEYBOARD_ACTION);
+}
