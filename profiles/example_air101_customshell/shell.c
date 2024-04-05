8cecdb74ac400d47e1a27e79c7eeae070dd6e3e5
igorkll <frerther@gmail.com>
2024-04-05 18:35:29 +0300

    backup

--- profiles/example_air101_customshell/shell.c
+++ profiles/example_air101_customshell/shell.c
@@ -2,7 +2,7 @@ void customshell_run() {
     while (true) {
         graphic_clear(color_black);
         graphic_drawText(1, 1, "custom shell", color_orange);
-        graphic_drawText(1, 2 + graphic_getFontHeight(), "example", color_red);
+        graphic_drawText(1, 2 + graphic_getFontSizeY(), "example", color_red);
         graphic_update();
         wait(1000);
     }
