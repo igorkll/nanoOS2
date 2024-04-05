68dadf8944695a09114ed62678e1d86ca6d55aba
igorkll <frerther@gmail.com>
2024-04-05 15:08:41 +0300

    Update drivers.h

--- profiles/test_KMR1441_SPI_V2/drivers.h
+++ profiles/test_KMR1441_SPI_V2/drivers.h
@@ -4,6 +4,6 @@
 #define SCREEN_RESX      128
 #define SCREEN_RESY      128
 #define SCREEN_DC   33
-#define SCREEN_DIN  34
-#define SCREEN_CLK  35
+#define SCREEN_DIN  23
+#define SCREEN_CLK  19
 #define SCREEN_RST  32
