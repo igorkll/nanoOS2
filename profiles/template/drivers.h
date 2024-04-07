1a93c605dc95dd3238b29e3c85b9feef9a46e7e4
igorkll <frerther@gmail.com>
2024-04-07 22:14:34 +0300

    а шо тут писать?

--- profiles/template/drivers.h
+++ profiles/template/drivers.h
@@ -17,7 +17,7 @@
 #define SCREEN_VOLTAGE    4 // 0 - 7
 #define SCREEN_CONTRASTE  0 // 0 - 3
 #define SCREEN_SPI        SPI2_HOST
-#define SCREEN_SPI_SPEED  10*1000*1000
+#define SCREEN_SPI_SPEED  2*1000*1000 //use 10*1000*1000 with the gridient turned on (it won't work on all screens)
 #define SCREEN_RST  7
 #define SCREEN_DC   8
 #define SCREEN_DIN  5
