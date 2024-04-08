f65c4e9acb36afbdc6f36a8f16ca7e661320cb00
igorkll <frerther@gmail.com>
2024-04-08 13:38:07 +0300

    Update palette.h

--- main/palette.h
+++ main/palette.h
@@ -1,11 +1,11 @@
 #define palette_splash_bg     0x0b8fc3
 #define palette_splash_text   0xffffff
-#define palette_splash_frame  0x00ffd7
+#define palette_splash_frame  0x00a2ff
 
 #define palette_title_text    0xffffff
 #define palette_title_bg      0x00e58c
 #define palette_title_line    0x00ff2d
 
 #define palette_menu_text     0xffffff
-#define palette_menu_select   0x00cd17
+#define palette_menu_select   0x73bf13
 #define palette_menu_bg       0x037007
