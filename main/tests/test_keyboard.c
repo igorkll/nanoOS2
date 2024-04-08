cd23e0889eeec75a11fd8d3c33ce8cb4af391270
igorkll <frerther@gmail.com>
2024-04-08 14:04:10 +0300

    Update test_keyboard.c

--- main/tests/test_keyboard.c
+++ main/tests/test_keyboard.c
@@ -12,7 +12,7 @@ void keyboardtest_run() {
     C_CLEAR(counts);
     C_CLEAR(states);
 
-    int rectSize = ((graphic_getFontSizeX() + 1) * 7) + 2;
+    int rectSize = (graphic_x() / 2) - 3;
     int rectSizeY = graphic_getFontSizeY() + 2;
 
     void drawButton(const char* title, int index, int x, int y, bool state) {
@@ -39,10 +39,10 @@ void keyboardtest_run() {
         bool isEnter = control_isEnter();
         bool isDown = control_isMoveButton(CONTROL_DOWN);
         drawButton("UP", 0, 2, 2, control_isMoveButton(CONTROL_UP));
-        drawButton("X", 1, 2, 2 + rectSizeY, isEnter);
-        drawButton("DOWN", 2, 2, 2 + (rectSizeY * 2), isDown);
+        drawButton("X", 1, 2, 2 + rectSizeY + 3, isEnter);
+        drawButton("DOWN", 2, 2, 2 + ((rectSizeY + 3) * 2), isDown);
         drawButton("LEFT", 3, 2 + rectSize + 2, 2, control_isMoveButton(CONTROL_LEFT));
-        drawButton("RIGHT", 4, 2 + rectSize + 2, 2 + rectSizeY, control_isMoveButton(CONTROL_RIGHT));
+        drawButton("RIGHT", 4, 2 + rectSize + 2, 2 + rectSizeY + 3, control_isMoveButton(CONTROL_RIGHT));
         graphic_drawText(1, graphic_y() - graphic_getFontSizeY() - 1, "X & DOWN - EXIT", color_mselect(color_white, color_red));
         graphic_update();
         if (isEnter && isDown) return;
