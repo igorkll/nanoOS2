2f77f77d006e82667931adc14c71269d9430572a
igorkll <frerther@gmail.com>
2024-04-09 16:17:48 +0300

    Update xmath.c

--- main/xmath.c
+++ main/xmath.c
@@ -1,19 +1,20 @@
 #include "main.h"
 #include "math.h"
 
-uint32_t lastFpsReadTime = 0;
-uint32_t sum = 0;
-uint16_t count = 0;
+static uint32_t lastFpsReadTime = 0;
+static uint32_t sum = 0;
+static uint16_t count = 0;
+static int fps = 0;
 int xmath_fpsCount(int dt) {
     uint32_t time = uptime();
     if (time - lastFpsReadTime >= 1000) {
         lastFpsReadTime = time;
-        int fps = sum / count;
+        fps = sum / count;
         sum = 0;
         count = 0;
-        return fps;
     }
 
     sum += nRound((1.0 / dt) * 1000.0);
     count++;
+    return fps;
 }
