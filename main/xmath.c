16acb57f8114b2aa065b02b59ffcf39eb24498e0
igorkll <frerther@gmail.com>
2024-04-09 16:24:29 +0300

    Update xmath.c

--- main/xmath.c
+++ main/xmath.c
@@ -4,13 +4,13 @@
 static uint32_t lastFpsReadTime = 0;
 static float sum = 0;
 static uint16_t count = 0;
-static int fps = 0;
+static int fps = -1;
 
 void xmath_fpsCountReset() {
     lastFpsReadTime = 0;
     sum = 0;
     count = 0;
-    fps = 0;
+    fps = -1;
 }
 
 int xmath_fpsCount(int dt) {
@@ -18,7 +18,7 @@ int xmath_fpsCount(int dt) {
     if (time - lastFpsReadTime >= 1000) {
         lastFpsReadTime = time;
         fps = -1;
-        if (count == 0) fps = nRound(sum / count);
+        if (count != 0) fps = nRound(sum / count);
         sum = 0;
         count = 0;
     }
