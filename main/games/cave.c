c6ad58e3e6674a06da0605f6be0b723ef3993bd6
igorkll <frerther@gmail.com>
2024-04-10 18:55:01 +0300

    Update cave.c

--- main/games/cave.c
+++ main/games/cave.c
@@ -84,7 +84,7 @@ static void drawCallback(int dt, float mul, void* param) {
 }
 
 static char move(struct Game* game, float x, float y) {
-    char chr = levelGet(nRound(game->playerPosX + x), nRound(game->playerPosY + y));
+    char chr = levelGet(game, nRound(game->playerPosX + x), nRound(game->playerPosY + y));
     if (chr != ' ') {
         game->playerPosX += x;
         game->playerPosY += y;
