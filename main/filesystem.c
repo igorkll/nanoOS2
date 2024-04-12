bec88763a6ffbe4a3d7d459ad2aff56c40dd7049
igorkll <frerther@gmail.com>
2024-04-12 13:59:28 +0300

    files/dirs count test

--- main/filesystem.c
+++ main/filesystem.c
@@ -3,6 +3,7 @@
 
 #include <esp_vfs.h>
 #include <esp_vfs_fat.h>
+#include <dirent.h>
 
 static esp_err_t fs_init_storage() {
     static wl_handle_t s_wl_handle = WL_INVALID_HANDLE;
@@ -49,4 +50,30 @@ int filesystem_size(const char* path) {
     struct stat state; 
     if (stat(path, &state) == 0) return state.st_size;
     return -1; 
+}
+
+static int _count(const char* path, bool files, bool dirs) {
+    DIR *dir = opendir(path);
+    if (dir == NULL) return -1;
+    int count = 0;
+    struct dirent *entry;
+    while ((entry = readdir(dir)) != NULL) {
+        if (entry->d_type == DT_DIR && strcmp(entry->d_name, ".") == 0) continue;
+        if (entry->d_type == DT_DIR && strcmp(entry->d_name, "..") == 0) continue;
+        count++;
+    }
+    closedir(dir);
+    return count;
+}
+
+int filesystem_fileCount(const char* path) {
+    return _count(path, true, false);
+}
+
+int filesystem_dirCount(const char* path) {
+    return _count(path, false, true);
+}
+
+int filesystem_objCount(const char* path) {
+    return _count(path, true, true);
 }
--- main/filesystem.h
+++ main/filesystem.h
@@ -3,3 +3,7 @@ bool filesystem_exists(const char *path);
 bool filesystem_isDirectory(const char *path);
 int filesystem_readFile(const char* path, void* buffer, int bufferLen);
 int filesystem_size(const char* path);
+
+int filesystem_fileCount(const char* path);
+int filesystem_dirCount(const char* path);
+int filesystem_objCount(const char* path);
