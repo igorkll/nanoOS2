16f5fbc8015fcf74d7cc87735a42392b4e7700cb
igorkll <frerther@gmail.com>
2024-04-15 18:12:32 +0300

    Update explorer.c

--- main/apps/explorer.c
+++ main/apps/explorer.c
@@ -31,22 +31,7 @@ static bool _recursive_explorer(const char* folder, char* open, struct ExplorerD
         menu.imgs = imglist;
 
         gui_menu(&menu);
-        if (menu.rightLeftState == 0) {
-            if (menu.current == objcount) {
-                C_FREE_LST(objlist, objcount);
-                return false;
-            }
-
-            char newPath[FILESYSTEM_PATH_LEN] = {0};
-            filesystem_concat(newPath, folder, objlist[menu.current]);
-            C_FREE_LST(objlist, objcount);
-            
-            if (filesystem_isDirectory(newPath)) {
-                if (_recursive_explorer(newPath, open, data)) return true;
-            } else {
-                
-            }
-        } else {
+        if (menu.rightLeftState == CONTROL_LEFT) {
             const char* strs[] = {"nothing to paste", "mkdir", "< back"};
             if (data->isCopy) {
                 if (data->isMove) {
@@ -79,6 +64,21 @@ static bool _recursive_explorer(const char* folder, char* open, struct ExplorerD
                         running = false;
                         break;
                 }
+            }
+        } else {
+            if (menu.current == objcount) {
+                C_FREE_LST(objlist, objcount);
+                return false;
+            }
+
+            char newPath[FILESYSTEM_PATH_LEN] = {0};
+            filesystem_concat(newPath, folder, objlist[menu.current]);
+            C_FREE_LST(objlist, objcount);
+            
+            if (!filesystem_isDirectory(newPath) || menu.rightLeftState == CONTROL_RIGHT) {
+                
+            } else {
+                if (_recursive_explorer(newPath, open, data)) return true;
             }
         }
     }
