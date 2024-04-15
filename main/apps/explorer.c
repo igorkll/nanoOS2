6350593eb9d74cc98009d1db2f314c0b4e932162
igorkll <frerther@gmail.com>
2024-04-15 17:01:14 +0300

    Update explorer.c

--- main/apps/explorer.c
+++ main/apps/explorer.c
@@ -1,9 +1,15 @@
 #include "../all.h"
 #include "explorer.h"
 
-static void _explorer(const char* folder, char* open) {
+static struct ExplorerData {
+    char* copy_path[FILESYSTEM_PATH_LEN];
+    bool isMove;
+};
+
+static bool _recursive_explorer(const char* folder, char* open, struct ExplorerData* data) {
     struct menuState menu = {
-        .title = folder
+        .title = folder,
+        .rightLeftControl = true
     };
     while (true) {
         uint16_t objcount = filesystem_objCount(folder);
@@ -24,21 +30,38 @@ static void _explorer(const char* folder, char* open) {
         menu.imgs = imglist;
 
         gui_menu(&menu);
-        if (menu.current == objcount) {
-            C_FREE_LST(objlist, objcount);
-            return;
-        }
+        if (menu.rightLeftControl == 0) {
+            if (menu.current == objcount) {
+                C_FREE_LST(objlist, objcount);
+                return;
+            }
 
-        char newPath[FILESYSTEM_PATH_LEN] = {0};
-        filesystem_concat(newPath, folder, objlist[menu.current]);
-        C_FREE_LST(objlist, objcount);
-        
-        if (filesystem_isDirectory(newPath)) {
-            _explorer(newPath, NULL);
-        } else {
+            char newPath[FILESYSTEM_PATH_LEN] = {0};
+            filesystem_concat(newPath, folder, objlist[menu.current]);
+            C_FREE_LST(objlist, objcount);
             
+            if (filesystem_isDirectory(newPath)) {
+                if (_recursive_explorer(newPath, open, data)) return true;
+            } else {
+                
+            }
+        } else {
+            const char* strs[] = {"mkdir", "back"};
+
+            struct menuState menu2 = {
+                .title = folder,
+                .pointsCount = 2,
+                .points = strs
+            };
         }
     }
+}
+
+static void _explorer(const char* folder, char* open) {
+    struct ExplorerData data = {
+        .isMove = false
+    };
+    _recursive_explorer(folder, open, &data);
 }
 
 void explorer_run() {
