983f0cf96bfe052e7ffe8dde1b183eb4ad6d6b4d
igorkll <frerther@gmail.com>
2024-04-16 20:04:23 +0300

    backup

--- profiles/test_KMR1441_SPI_V2/drivers.h
+++ profiles/test_KMR1441_SPI_V2/drivers.h
@@ -1,13 +1,13 @@
 //screen_ST7735.c
 #define SCREEN_SPI       HSPI_HOST
-#define SCREEN_SPI_SPEED 20000000
+#define SCREEN_SPI_SPEED 26000000
 #define SCREEN_RESX      128
 #define SCREEN_RESY      128
 #define SCREEN_OFFSET_X  32
 #define SCREEN_OFFSET_Y  0
 #define SCREEN_DC   33
 #define SCREEN_DIN  23
-#define SCREEN_CLK  19
+#define SCREEN_CLK  18
 #define SCREEN_RST  32
 
 //keyboard_simple.c
