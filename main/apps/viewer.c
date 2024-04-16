51ab49f5cb3ac455a79e9b9906df1c1885868804
igorkll <frerther@gmail.com>
2024-04-16 18:14:11 +0300

    backup

--- main/apps/viewer.c
+++ main/apps/viewer.c
@@ -5,10 +5,7 @@ void viewer_run() {
 
 }
 
-void viewer_open(const char* path) {
-    uint8_t cropX = graphic_getCropX();
-    uint8_t cropY = graphic_getCropY();
-    graphic_setCrop(1);
+void viewer_draw(const char* path) {
     uint32_t* img = graphic_loadImage(path);
     graphic_clear(color_getGray(graphic_dumpPrimaryColor(img)) < 8 ? color_white : color_black);
 
@@ -25,7 +22,13 @@ void viewer_open(const char* path) {
     free(img);
     graphic_draw(graphic_centerX(resizedImage[0]), graphic_centerY(resizedImage[1]), resizedImage);
     free(resizedImage);
+}
 
+void viewer_open(const char* path) {
+    uint8_t cropX = graphic_getCropX();
+    uint8_t cropY = graphic_getCropY();
+    graphic_setCrop(1);
+    viewer_draw(path);
     graphic_update();
     control_waitEnter();
     graphic_setCropXY(cropX, cropY);
--- main/apps/viewer.h
+++ main/apps/viewer.h
@@ -1,2 +1,3 @@
 void viewer_run();
+void viewer_draw(const char* path);
 void viewer_open(const char* path);
