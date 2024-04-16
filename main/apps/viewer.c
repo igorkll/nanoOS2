6771c6c83badbff8d1fd657d96c81892a9933f4b
igorkll <frerther@gmail.com>
2024-04-16 18:12:54 +0300

    Update viewer.c

--- main/apps/viewer.c
+++ main/apps/viewer.c
@@ -11,10 +11,21 @@ void viewer_open(const char* path) {
     graphic_setCrop(1);
     uint32_t* img = graphic_loadImage(path);
     graphic_clear(color_getGray(graphic_dumpPrimaryColor(img)) < 8 ? color_white : color_black);
-    uint32_t* resizedImage = graphic_resize(img, graphic_x(), graphic_y());
+
+    float mul;
+    uint16_t rx = graphic_x();
+    uint16_t ry = graphic_y();
+    if (ry > rx) {
+        mul = rx / img[0];
+    } else {
+        mul = ry / img[1];
+    }
+
+    uint32_t* resizedImage = graphic_resize(img, img[0] * mul, img[1] * mul);
     free(img);
     graphic_draw(graphic_centerX(resizedImage[0]), graphic_centerY(resizedImage[1]), resizedImage);
     free(resizedImage);
+
     graphic_update();
     control_waitEnter();
     graphic_setCropXY(cropX, cropY);
