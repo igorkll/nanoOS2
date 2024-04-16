27bf97680f5a4b7e078a6bea631106a007627570
igorkll <frerther@gmail.com>
2024-04-16 20:43:48 +0300

    KMR1441_SPI_V2 speed changed

--- profiles/test_KMR1441_SPI_V2/drivers.h
+++ profiles/test_KMR1441_SPI_V2/drivers.h
@@ -1,6 +1,6 @@
 //screen_ST7735.c
 #define SCREEN_SPI       HSPI_HOST
-#define SCREEN_SPI_SPEED 26000000
+#define SCREEN_SPI_SPEED 32000000 //if your screen doesn't work, try installing 10000000 here
 #define SCREEN_RESX      128
 #define SCREEN_RESY      128
 #define SCREEN_OFFSET_X  32
