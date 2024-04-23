cefa916e8197ed7157ed82861d2ce545c331a7f5
igorkll <frerther@gmail.com>
2024-04-23 17:46:06 +0300

    backup

--- drivers/screen_SPFD5408.c
+++ drivers/screen_SPFD5408.c
@@ -2,17 +2,23 @@
 #include "../main/color.h"
 #include "../main/drivers/screen.h"
 
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_vendor.h"
+#include "esp_lcd_panel_ops.h"
 
 #ifndef SCREEN_RESX
-    #define SCREEN_RESX 320
+    #define SCREEN_RESX 5
 #endif
 
 #ifndef SCREEN_RESY
-    #define SCREEN_RESY 240
+    #define SCREEN_RESY 5
+#endif
+
+#ifndef SCREEN_SPEED
+    #define SCREEN_SPEED 1000000
 #endif
 
 static uint8_t buffer[SCREEN_RESX * SCREEN_RESY * 2];
-static uint8_t bus_pins[] = SCREEN_BUS;
 
 // --------------------------------
 
@@ -58,7 +64,64 @@ int screen_y() {
 }
 
 esp_err_t screen_init() {
-    pin(SCREEN_DC, GPIO_MODE_DEF_OUTPUT);
+    #ifdef SCREEN_RST
+        pin(SCREEN_RST, GPIO_MODE_DEF_OUTPUT);
+        gpio_set_level(SCREEN_RST, false);
+        wait(100);
+        gpio_set_level(SCREEN_RST, true);
+    #endif
+
+    bool flush_ready(esp_lcd_panel_io_handle_t panel_io, esp_lcd_panel_io_event_data_t *edata, void *user_ctx) {
+        return false;
+    }
+    
+    esp_lcd_i80_bus_handle_t i80_bus = NULL;
+    esp_lcd_i80_bus_config_t bus_config = {
+        .clk_src = LCD_CLK_SRC_DEFAULT,
+        .dc_gpio_num = SCREEN_DC,
+        .wr_gpio_num = SCREEN_WR,
+        .data_gpio_nums = SCREEN_BUS,
+        .bus_width = SCREEN_BUS_WIDTH,
+        .max_transfer_bytes = sizeof(buffer)
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_i80_bus(&bus_config, &i80_bus));
+
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i80_config_t io_config = {
+        #ifdef SCREEN_CS
+            .cs_gpio_num = SCREEN_CS,
+        #endif
+        .pclk_hz = SCREEN_SPEED,
+        .trans_queue_depth = 10,
+        .dc_levels = {
+            .dc_idle_level = 0,
+            .dc_cmd_level = 0,
+            .dc_dummy_level = 0,
+            .dc_data_level = 1,
+        },
+        .flags = {
+            .swap_color_bytes = false,
+        },
+        .on_color_trans_done = flush_ready,
+        .user_ctx = NULL,
+        .lcd_cmd_bits = 8,
+        .lcd_param_bits = 8,
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i80(i80_bus, &io_config, &io_handle));
+
+    esp_lcd_panel_io_tx_param(io_handle, 0xF2, (uint8_t[]) { 0 }, 1); // 3Gamma function disable
+    esp_lcd_panel_io_tx_param(io_handle, 0x26, (uint8_t[]) { 1 }, 1); // Gamma curve 1 selected
+    esp_lcd_panel_io_tx_param(io_handle, 0xE0, (uint8_t[]) {          // Set positive gamma
+        0x0F, 0x31, 0x2B, 0x0C, 0x0E, 0x08, 0x4E, 0xF1, 0x37, 0x07, 0x10, 0x03, 0x0E, 0x09, 0x00 }, 15);
+    esp_lcd_panel_io_tx_param(io_handle, 0xE1, (uint8_t[]) {          // Set negative gamma
+        0x00, 0x0E, 0x14, 0x03, 0x11, 0x07, 0x31, 0xC1, 0x48, 0x08, 0x0F, 0x0C, 0x31, 0x36, 0x0F }, 15);
+
+    while (true) {
+        printf("TRY SEND\n");
+        esp_lcd_panel_io_tx_color(io_handle, -1, esp_random(), 1);
+        printf("END SEND\n");
+        wait(1000);
+    }
     
 
     return ESP_OK;
