8307f748dc52f98a8bfa58f745c3aa7e96a10bcd
igorkll <frerther@gmail.com>
2024-05-01 17:40:48 +0300

    backup

--- main/hidden_tools/lua.c
+++ main/hidden_tools/lua.c
@@ -3,36 +3,7 @@
 #include "../../libs/lua53/lua.h"
 #include "../../libs/lua53/lauxlib.h"
 #include "../../libs/lua53/lualib.h"
-
-// ----------------------- bind utils
-
-static uint8_t increment(uint8_t* v) {
-    (*v)++;
-    return *v;
-}
-
-#define LUA_ARG_INT luaL_checkinteger(L, increment(&i))
-#define LUA_RET_INT(val) lua_pushinteger(L, val)
-
-#define LUA_BIND_VOID(func, name, args) { \
-    int c_func(lua_State* L) { \
-        uint8_t i = 0; \
-        func args; \
-        return 0; \
-    } \
-    lua_pushcfunction(lua, c_func); \
-    lua_setglobal(lua, name); \
-}
-
-#define LUA_BIND_RETR(func, name, args, returnType) { \
-    int c_func(lua_State* L) { \
-        uint8_t i = 0; \
-        returnType(func args) \
-        return 1; \
-    } \
-    lua_pushcfunction(lua, c_func); \
-    lua_setglobal(lua, name); \
-}
+#include "../service/lua_binder.h"
 
 // -----------------------
 
@@ -42,6 +13,9 @@ void lua_open(const char* path) {
     
     lua_State* lua = luaL_newstate();
     luaL_openlibs(lua);
+    LUA_BIND_VOID(wait, (LUA_ARG_INT));
+    LUA_BIND_RETR(control_getState, (LUA_ARG_INT), LUA_RET_BOOL);
+
     if (luaL_dofile(lua, path)) {
         graphic_printf(color_red, "error: %s", lua_tostring(lua, -1));
         graphic_update();
---
+++ main/service/lua_binder.h
@@ -0,0 +1,29 @@
+static uint8_t increment(uint8_t* v) {
+    (*v)++;
+    return *v;
+}
+
+#define LUA_ARG_INT luaL_checkinteger(L, increment(&i))
+#define LUA_ARG_BOOL lua_toboolean(L, increment(&i))
+#define LUA_RET_INT(val) lua_pushinteger(L, val)
+#define LUA_RET_BOOL(val) lua_pushboolean(L, val)
+
+#define LUA_BIND_VOID(func, args) { \
+    int c_func(lua_State* L) { \
+        uint8_t i = 0; \
+        func args; \
+        return 0; \
+    } \
+    lua_pushcfunction(lua, c_func); \
+    lua_setglobal(lua, #func); \
+}
+
+#define LUA_BIND_RETR(func, args, returnType) { \
+    int c_func(lua_State* L) { \
+        uint8_t i = 0; \
+        returnType(func args); \
+        return 1; \
+    } \
+    lua_pushcfunction(lua, c_func); \
+    lua_setglobal(lua, #func); \
+}
