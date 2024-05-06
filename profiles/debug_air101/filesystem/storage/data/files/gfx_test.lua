abe108afd52b64badbc5c97663d1983779563053
igorkll <frerther@gmail.com>
2024-05-06 20:09:18 +0300

    backup

--- profiles/debug_air101/filesystem/storage/data/files/gfx_test.lua
+++ profiles/debug_air101/filesystem/storage/data/files/gfx_test.lua
@@ -1,4 +1,5 @@
 graphic_clear(0x0000ee)
+graphic_setCrop(1)
 while true do
     for i = 1, 100 do
         graphic_drawPixel(math.random(0, graphic_x() - 1), math.random(0, graphic_y() - 1), math.random(0, 0xffffff))
