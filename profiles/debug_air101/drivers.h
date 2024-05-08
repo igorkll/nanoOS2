774f3cafd0dd5605e578fc83f8c2be3828571af4
igorkll <frerther@gmail.com>
2024-05-08 14:35:28 +0300

    debug_air101 sdcard support added

--- profiles/debug_air101/drivers.h
+++ profiles/debug_air101/drivers.h
@@ -1,5 +1,5 @@
-//screen_custom_driver_test.c
-#define SCREEN_SPI       SPI2_HOST
+//screen_ST7735.c
+#define SCREEN_BASESPI
 #define SCREEN_SPI_SPEED 40000000
 #define SCREEN_RESX      160
 #define SCREEN_RESY      80
@@ -7,9 +7,7 @@
 #define SCREEN_OFFSET_Y  26
 #define SCREEN_HARDWARE_INVERT_COLORS
 #define SCREEN_DC   6
-#define SCREEN_DIN  3
 #define SCREEN_CS   7
-#define SCREEN_CLK  2
 #define SCREEN_RST  10
 #define SCREEN_BL   11
 
